27b71a45f2d8246165c82c2943ef24bccda983d2
Rayleigh962 <yaseencosmic@gmail.com>
2020-08-23 14:38:54 +0530

    added basic server

---
+++ data.js
@@ -0,0 +1,18 @@
+const data = [
+	{
+		id: 1,
+		name: 'bob',
+		status: 'unactive'
+	},
+	{
+		id: 2,
+		name: 'jack',
+		status: 'unactive'
+	},
+	{
+		id: 3,
+		name: 'martin',
+		status: 'active'
+	}
+];
+module.exports = data;
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const data = require('./data.js');
+
+app.use(cors());
+
+//set static
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// gets all member
+app.get('/api/members', (req, res) => {
+	const { id } = req.query;
+	res.send(data[id - 1]);
+});
+
+app.listen(PORT, err => {
+	if (err) {
+		console.log(err);
+		return;
+	}
+	console.log(`listening at port: ${PORT}`);
+});
